bf7bee4718df05590f08b3337496ebb3b3606e65
4ions <leovalenciasantana@gmail.com>
2021-07-12 17:39:15 -0500

    Print without a loop

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.Write(str);
+		Console.Write(str);
+		Console.WriteLine(str);
+        System.Console.WriteLine(str.Substring(0,9));
+    }
+}
