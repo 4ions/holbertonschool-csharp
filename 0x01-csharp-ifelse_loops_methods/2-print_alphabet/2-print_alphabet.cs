220d4a106cd28f0bb659411a34d13d836dd99617
4ions <leovalenciasantana@gmail.com>
2021-07-13 14:56:56 -0500

    Print alphabet

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            alphabet = 
+            for (char i = 'a'; i <= 'z'; i++)
+            {   
+                Console.Write(i);
+                
+            }
+        }
+    }
+}
