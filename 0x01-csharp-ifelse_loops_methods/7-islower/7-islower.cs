4e55086e0ec21f797c03b9d460919529c573e74e
4ions <leovalenciasantana@gmail.com>
2021-07-13 16:06:08 -0500

    Is lower or upper

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _7_islower
+{
+    class Character
+    {
+        public static bool IsLower(char c){
+            string upper = "aabcdefghijklmnopqrstuvwxyz";
+            if (upper.Contains(c)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+}
