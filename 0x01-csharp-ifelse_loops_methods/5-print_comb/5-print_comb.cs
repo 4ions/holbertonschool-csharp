9c404b4a1cc281f9ee94191abbae47317404a4bc
4ions <leovalenciasantana@gmail.com>
2021-07-13 15:41:35 -0500

    Print comb

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            for (int i = 1; i <= 99; i++)
+            {
+                number = i / 10;
+                if (i < 99)
+                    Console.Write($"{number}{i % 10}, ");
+
+                if (i == 99){
+                    Console.Write($"{i}\n");
+                }
+            }
+        }
+    }
+}
