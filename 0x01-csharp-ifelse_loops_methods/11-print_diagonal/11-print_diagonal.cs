0c75167327d16e2dd826906af1f40c2fc56756e4
4ions <leovalenciasantana@gmail.com>
2021-07-13 16:34:04 -0500

    Print diagonal

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _11_print_diagonal
+{
+    class Line
+    {
+        public static void PrintDiagonal(int length){
+            if (length == 0 || length < 0)
+            {
+                Console.WriteLine();
+                return;
+            }
+
+            string space = "";
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine($"{space}\\");
+                space += " ";
+            }
+            Console.WriteLine();
+        }
+    }
+}
