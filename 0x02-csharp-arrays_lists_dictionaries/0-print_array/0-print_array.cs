6da086d7cc4aa46d8d8e6d69e3c8b9a367d465e9
4ions <leovalenciasantana@gmail.com>
2021-07-14 15:41:25 -0500

    Change order of if

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -6,20 +6,20 @@ namespace _0_print_array
     {
         public static int[] CreatePrint(int size){
             
+            if (size == 0){
+                Console.WriteLine("");
+                return new int[0];
+            }
             if (size < 0){
                 Console.WriteLine("Size cannot be negative");
                 return null;
-            }
-            if (size == 0){
-                Console.WriteLine();
-                return new int[0];
             }
             int[] newArray = new int[size];
 
             for (int i = 0; i < size; i++){
                 newArray[i] = i;
                 if (i != size -1)
-                    Console.Write($"{i} ");
+                    Console.Write($"{i}*");
                 else
                     Console.Write(i);
                 
