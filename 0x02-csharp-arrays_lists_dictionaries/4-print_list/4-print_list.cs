5a1655fc0cf08e7ac8201f3f1e1ffdf8a435e3f3
4ions <leovalenciasantana@gmail.com>
2021-07-14 17:26:39 -0500

    Added if

--- 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -4,8 +4,13 @@ using System.Collections.Generic;
 class List
 {
     public static List<int> CreatePrint(int size){
+        if (size == 0)
+		{
+			Console.WriteLine("");
+			return new List<int>();
+		}
         if (size < 0){
-            Console.WriteLine("size cannot be negative");
+            Console.WriteLine("Size cannot be negative");
             return null;
         }
         List<int> list = new List<int>();
