126eb15055ea09b5965547f99a9196c994bfabe1
4ions <leovalenciasantana@gmail.com>
2021-07-14 15:38:51 -0500

    Changed the return if size == 0

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -5,15 +5,15 @@ namespace _0_print_array
     class Array
     {
         public static int[] CreatePrint(int size){
+            
             if (size < 0){
                 Console.WriteLine("Size cannot be negative");
                 return null;
-            }else if (size == 0){
-                int[] ceroArray = new int[0];
-                Console.WriteLine();
-                return ceroArray;
             }
-
+            if (size == 0){
+                Console.WriteLine();
+                return new int[0];
+            }
             int[] newArray = new int[size];
 
             for (int i = 0; i < size; i++){
