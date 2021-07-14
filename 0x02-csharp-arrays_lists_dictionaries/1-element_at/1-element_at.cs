cded68896987201bc4660d4ca47130165f8d9eab
4ions <leovalenciasantana@gmail.com>
2021-07-14 15:56:44 -0500

    added - 1 in array.length

--- 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -3,7 +3,7 @@
 class Array
 {
     public static int elementAt(int[] array, int index){
-        if (index > array.Length || index < 0)
+        if (index > array.Length - 1 || index < 0)
         {
             Console.WriteLine("Index out of range");
             return -1;
