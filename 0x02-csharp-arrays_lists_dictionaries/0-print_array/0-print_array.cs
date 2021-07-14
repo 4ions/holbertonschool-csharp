9158e13c8f122a626f77022573e1e7c35068dac9
4ions <leovalenciasantana@gmail.com>
2021-07-14 15:27:36 -0500

    Print array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _0_print_array
+{
+    class Array
+    {
+        public static int[] CreatePrint(int size){
+            if (size < 0){
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }else if (size == 0){
+                int[] ceroArray = new int[0];
+                Console.WriteLine();
+                return ceroArray;
+            }
+
+            int[] newArray = new int[size];
+
+            for (int i = 0; i < size; i++){
+                Console.Write($"{i} ");
+                newArray[i] = i;
+            }
+            Console.WriteLine();
+            return newArray;
+            
+
+        }
+    }
+}
