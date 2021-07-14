91f38756552b356d21340258ef2017fe96b92b33
4ions <leovalenciasantana@gmail.com>
2021-07-14 15:46:21 -0500

    remove namespace

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,29 +1,26 @@
 ﻿using System;
 
-namespace _0_print_array
+class Array
 {
-    class Array
-    {
-        public static int[] CreatePrint(int size){
-            
-            if (size == 0){
-                Console.WriteLine("");
-                return new int[0];
-            }
-            if (size < 0){
-                Console.WriteLine("Size cannot be negative");
-                return null;
-            }
-            int[] newArray = new int[size];
-
-            for (int i = 0; i < size; i++)
-                newArray[i] = i;
-                              
-            
-            Console.WriteLine(string.Join(" ", newArray));
-            return newArray;
-            
-
+    public static int[] CreatePrint(int size){
+        
+        if (size == 0){
+            Console.WriteLine("");
+            return new int[0];
         }
+        if (size < 0){
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int[] newArray = new int[size];
+
+        for (int i = 0; i < size; i++)
+            newArray[i] = i;
+                            
+        
+        Console.WriteLine(string.Join(" ", newArray));
+        return newArray;
+        
+
     }
 }
