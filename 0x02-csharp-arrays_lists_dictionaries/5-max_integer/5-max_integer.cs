d1b5ded8d759eb287f1181e4fd2e401197ba7b01
4ions <leovalenciasantana@gmail.com>
2021-07-14 18:42:29 -0500

    Added WriteLine

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -5,6 +5,7 @@ class List
 {
     public static int MaxInteger(List<int> myList){
         if (myList.Count == 0){
+            Console.WriteLine("List is empty");
             return -1;
         }
         int max = 0;
