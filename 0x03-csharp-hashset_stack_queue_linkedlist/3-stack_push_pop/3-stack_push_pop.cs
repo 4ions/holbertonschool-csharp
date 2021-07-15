12f7ace969ee1c57d72623c9fe2f6093358a4a3f
4ions <leovalenciasantana@gmail.com>
2021-07-15 11:20:46 -0500

    Stack push and pop

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search){
+        if (aStack.Count <= 0){
+            System.Console.WriteLine("Stack is empty");
+        }
+        int numberOfItems = 0;
+        foreach(string item in aStack){
+            numberOfItems += 1;
+        }
+        System.Console.WriteLine($"Number of items: {numberOfItems}");
+        System.Console.WriteLine($"Top item: {aStack.Peek()}");
+        System.Console.WriteLine($"Stack contains \"{search}\": {aStack.Contains(search)}");
+        if (aStack.Contains(search)){
+            while (aStack.Contains(search))
+                aStack.Pop();
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
