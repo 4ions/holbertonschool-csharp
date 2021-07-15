683cf8356f399e818f2bb37394a80fbab9f8eec0
4ions <leovalenciasantana@gmail.com>
2021-07-15 14:56:20 -0500

    Linked list delete

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index){
+
+        int counter = 0;
+        LinkedListNode<int> current = myLList.First;
+
+        while(current != null){
+            if (counter == index){
+                myLList.Remove(current);
+            }
+            counter++;
+            current = current.Next;
+        }
+    }
+}
