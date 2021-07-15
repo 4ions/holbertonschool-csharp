ef8713441f4f2ed1d1aff1b2431e386be84be24c
4ions <leovalenciasantana@gmail.com>
2021-07-15 14:29:31 -0500

    Lined list sum of all data

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList){
+        
+        int Sum = 0;
+
+        foreach(int item in myLList){
+            Sum = Sum + item;
+        }
+
+        return Sum;
+    }
+}
