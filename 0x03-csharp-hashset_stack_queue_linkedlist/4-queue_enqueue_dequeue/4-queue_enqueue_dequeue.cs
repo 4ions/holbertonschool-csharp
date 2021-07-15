11089a5122294bd40e6176e2cac2046b82b359c4
4ions <leovalenciasantana@gmail.com>
2021-07-15 11:35:54 -0500

    Queue enqueue and deuqueue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search){
+
+        System.Console.WriteLine($"Number of items: {aQueue.Count}");
+
+        if (aQueue.Count <= 0)
+            System.Console.WriteLine("Queue is empty");
+        else
+            System.Console.WriteLine($"First item: {aQueue.Peek()}");
+        
+        aQueue.Enqueue(newItem);
+
+        System.Console.WriteLine($"Queue contains \"{search}\": {aQueue.Contains(search)}");
+        if (aQueue.Contains(search))
+        {
+            while (aQueue.Contains(search)){
+                aQueue.Dequeue();
+            }
+        }
+        return aQueue;
+
+    }
+}
