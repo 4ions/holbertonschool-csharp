6d1895d6f02c3e7a5ab2dd38c4d78ceaa89338c3
4ions <leovalenciasantana@gmail.com>
2021-07-14 20:18:45 -0500

    Best score in a dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList){
+        if (myList.Count == 0){
+            return "None";
+        }
+
+        int maxScore = 0;
+        string key = "";
+        foreach (KeyValuePair<string, int> entry in myList){
+            if (maxScore < entry.Value){
+                maxScore = entry.Value;
+                key = entry.Key;
+            }
+        }
+        return key;
+    }
+}
