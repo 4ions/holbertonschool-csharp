6f8f6615c8079f39ec05afd63bb140ab02ee1cd2
4ions <leovalenciasantana@gmail.com>
2021-07-15 12:10:49 -0500

    Fix

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search){
+
+        System.Console.WriteLine($"Number of items: {aStack.Count}");
+
+        
         if (aStack.Count <= 0){
             System.Console.WriteLine("Stack is empty");
+        }else {
+            System.Console.WriteLine($"Top item: {aStack.Peek()}");
         }
-        int numberOfItems = 0;
-        foreach(string item in aStack){
-            numberOfItems += 1;
-        }
-        System.Console.WriteLine($"Number of items: {numberOfItems}");
-        System.Console.WriteLine($"Top item: {aStack.Peek()}");
+
+        
         System.Console.WriteLine($"Stack contains \"{search}\": {aStack.Contains(search)}");
         if (aStack.Contains(search)){
             while (aStack.Contains(search))
