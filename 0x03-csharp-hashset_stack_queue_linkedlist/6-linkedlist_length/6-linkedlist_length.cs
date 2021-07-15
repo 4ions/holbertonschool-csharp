b7696b2df57025eb2b8769001c499ef4a784900e
4ions <leovalenciasantana@gmail.com>
2021-07-15 11:52:27 -0500

    Linked list length

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList){
+        int sum = 0;
+        foreach (int i in myLList){
+            sum += 1;
+        }
+        return sum;
+    }
+}
