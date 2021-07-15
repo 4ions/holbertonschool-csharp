a58513b23841b90b818bb31ec3a42a53521fbdb3
4ions <leovalenciasantana@gmail.com>
2021-07-15 10:51:13 -0500

    Different elements in two lists

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2){
+        List<int> noSame = new List<int>();
+        foreach (int i in list1){
+            if (!list2.Contains(i)){
+                noSame.Add(i);
+
+            }
+        }
+        foreach (int i in list2){
+            if (!list1.Contains(i)){
+                noSame.Add(i);
+            }
+        }
+        noSame.Sort();
+        return noSame;
+    }
+}
