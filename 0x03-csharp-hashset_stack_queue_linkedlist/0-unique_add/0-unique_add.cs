7dca6b9789e6b93c1110d80a3382702997cea5fc
4ions <leovalenciasantana@gmail.com>
2021-07-15 09:23:10 -0500

    Unique add using hashSet because this only add unique values

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static int Sum(List<int> myList){
+        HashSet<int> myHashSet = new HashSet<int>();
+        foreach(int i in myList){
+            myHashSet.Add(i);
+        }
+        int sum = 0;
+        foreach(int i in myHashSet){
+            sum += i;
+        }
+        return sum;
+    }
+}
