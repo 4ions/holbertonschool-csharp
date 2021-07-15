391ea2cc88ca68ae36d5daac5359aa8806f506f2
4ions <leovalenciasantana@gmail.com>
2021-07-15 14:24:50 -0500

    Linked list get node

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n){
+        if (myLList.Count <= 0 || n > myLList.Count){
+            return 0;
+        }else{
+        int counter = 0;
+        int value = 0;
+        foreach(int i in myLList){
+            if (counter == n){
+                value = i;
+            }
+            counter++;
+        }
+        return value;
+        }
+    }
+}
