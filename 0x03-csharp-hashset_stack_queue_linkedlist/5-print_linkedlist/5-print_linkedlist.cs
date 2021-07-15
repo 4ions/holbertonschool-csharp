a9cb81dc097b6d7596abf3e6d67cb6db3d6a5f4d
4ions <leovalenciasantana@gmail.com>
2021-07-15 11:48:26 -0500

    Print linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size){
+        LinkedList<int> list = new LinkedList<int>();
+
+        if (size < 0){
+            return new LinkedList<int>();
+        }
+
+        for (int i = 0; i < size; i++){
+            list.AddLast(i);
+        }
+        foreach(int i in list){
+            System.Console.WriteLine(i);
+        }
+
+        return list;
+    }
+}
