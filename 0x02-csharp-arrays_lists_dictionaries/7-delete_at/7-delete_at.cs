4e92f54f750dea3253c5c059acea73c53e5da048
4ions <leovalenciasantana@gmail.com>
2021-07-14 19:27:46 -0500

    Delete at

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -5,16 +5,13 @@ class List
 {
     public static List<int> DeleteAt(List<int> myList, int index){
 
-        if (index > myList.Count || index < 0)
+        if (index >= myList.Count || index < 0)
         {
             System.Console.WriteLine("Index is out of range");
             return myList;
         }
-        for(int i = index; i < myList.Count; i++){
-            if (index == i)
-                myList.Remove(myList[i]);
-        }
-
+        
+        myList.Remove(myList[index]);
         return myList;
     }
 }
