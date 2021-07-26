2c7baa086c40089e7361788841b74c71b3b621db
4ions <leovalenciasantana@gmail.com>
2021-07-26 14:06:55 -0500

    0-safe_list_print

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n){
+        int count = 0;
+        try {
+            for (int i = 0; i < n; i++)
+            {
+                System.Console.WriteLine(myList[i]);
+                count++;
+
+            }
+        }
+        catch {
+            return count;
+        }
+
+        return count;
+    }
+}
