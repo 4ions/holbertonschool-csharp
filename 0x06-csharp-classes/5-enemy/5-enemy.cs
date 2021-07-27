5a44edcff1c3149c4709642977cf808e7df846ea
4ions <leovalenciasantana@gmail.com>
2021-07-27 13:55:27 -0500

    .\5-enemy\

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Enemies{
+
+    ///<summary> public class zombie</summary>
+    public class Zombie {
+
+        ///<summary> public variable health </summary>
+        public int health;
+
+        ///<summary> private string name </summary>
+        private string name = "(No name)";
+        ///<summary> public constructor: public Zombie() Initializes in 0 </summary>
+        public Zombie(){
+            health = 0;
+        }    
+
+        ///<summary> public string Name to set and get values </summary>
+        public string Name {
+            get => name;
+            set => name = value; 
+        }
+
+        ///<summary> public constructor: public Zombie that recives value to set in health</summary>
+        public Zombie(int value){
+
+            if (value >= 0){
+                health = value;
+            }
+            else{
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+
+        ///<summary> public function to return the value of health </summary>
+        public int GetHealth(){
+            return health;
+        }
+
+        ///<summary> method to override Tostring()</summary>
+        public override string ToString(){
+            return $"Zombie Name: {name} / Total Health: {health}";
+        }
+
+        
+
+
+    }
+}
