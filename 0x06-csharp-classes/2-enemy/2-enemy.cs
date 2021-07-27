4a4e4de4d75d0f6044123a213bbd87f2bc8e0ce0
4ions <leovalenciasantana@gmail.com>
2021-07-27 13:00:38 -0500

    Added xml comments

--- 0x06-csharp-classes/2-enemy/2-enemy.cs
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 
 namespace Enemies{
+    ///<summary> public class zombie</summary>
+
     public class Zombie {
+
+        ///<summary> public variable health </summary>
         public int health;
 
+        ///<summary> public constructor: public Zombie() Initializes in 0 </summary>
         public Zombie(){
             health = 0;
         }     
 
+        ///<summary> public constructor: public Zombie that recives value to set in health</summary>
         public Zombie(int value){
 
             if (value >= 0){
