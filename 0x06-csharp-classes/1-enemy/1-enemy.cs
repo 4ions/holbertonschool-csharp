b2e2fc3951a79238210fe4d88c32a34123a6445d
4ions <leovalenciasantana@gmail.com>
2021-07-27 12:58:33 -0500

    Added xml comments

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -4,8 +4,9 @@ namespace Enemies{
     ///<summary> public class zombie</summary>
 
     public class Zombie {
+        ///<summary> public variable health </summary>
         public int health;
-        /// <summary> public constructor: public Zombie() Initializes in 0 </summary>
+        ///<summary> public constructor: public Zombie() Initializes in 0 </summary>
         public Zombie(){
             health = 0;
         }     
