ac711ab53a04f399f06e06117f79cf4cb47cde20
4ions <leovalenciasantana@gmail.com>
2021-08-02 10:58:02 -0500

    .\1-instance\

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj {
+    /// <summary> returns True if the object is an instance of, or return False</summary>
+    public static bool IsInstanceOfArray(object obj){
+        return (typeof(Array).IsInstanceOfType(obj));
+    }
+
+}
