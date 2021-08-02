f3cd0a0c31862b63e4c408f5d485a0dd4c4a48a2
4ions <leovalenciasantana@gmail.com>
2021-08-02 11:05:55 -0500

    .\2-subclass\

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Obj
+{
+    ///<summary> True if the object is instance of a class from the specified class </summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType){
+        return (derivedType.IsSubclassOf(baseType));
+    }
+}
