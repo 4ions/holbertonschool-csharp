39f22124fafaea5cd7c94c331621051a5c0e8fa6
4ions <leovalenciasantana@gmail.com>
2021-08-25 15:23:16 -0500

    Fixed return

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,23 +1,24 @@
 ﻿using System;
 
-/// <summary> Class VectorMath </summary>
+///<summary> Class VectorMath </summary>
 class VectorMath
 {
-    /// <summary> method that calculates 2d and 3d vector's and returns its magnitud </summary>
+    ///<summary> method that calculates 2d and 3d vector's and returns its magnitud </summary>
     public static double Magnitude(double[] vector){
         int size = vector.Length;
-        double result = -1;
+        double result = 0;
         if (size == 2 || size == 3){
             foreach (int i in vector)
             {
                 result += i * i;
             }
 
-            result = Math.Round(Math.Sqrt(result));
+            result = Math.Round(Math.Sqrt(result), 2);
+            return result;
         }
         
 
-        return result;
+        return -1;
     }
 }
 
