77115906ebe12f6fb81421ed15684f1281631844
4ions <leovalenciasantana@gmail.com>
2021-08-26 12:41:58 -0500

    .\14-matrix_addition\

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -7,7 +7,7 @@ class MatrixMath
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
         double[,] result = new double[matrix1.GetLength(0),matrix1.GetLength(0)];
-        if (matrix.GetLength(1) == matrix2.GetLength(1) && matrix1.GetLength(0) == matrix2.GetLength(0) && (matrix2.GetLength(0) == 3 || matrix1.GetLength(0) == 2)){
+        if (matrix1.GetLength(1) == matrix2.GetLength(1) && matrix1.GetLength(0) == matrix2.GetLength(0) && (matrix2.GetLength(0) == 3 || matrix1.GetLength(0) == 2)){
             for (int i = 0; i < matrix1.GetLength(0); i++){
                 for (int j = 0; j < matrix1.GetLength(0); j++)
                 {
