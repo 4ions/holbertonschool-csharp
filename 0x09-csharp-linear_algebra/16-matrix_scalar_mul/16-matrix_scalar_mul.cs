496bad59b40070262a33ac21835862176886a273
4ions <leovalenciasantana@gmail.com>
2021-08-26 14:07:41 -0500

    .\16-matrix_scalar_mul\

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -5,7 +5,7 @@ class MatrixMath
 {
     ///<summary> Method scalar multyplicate matrix</summary>
     public static double[,] MultiplyScalar(double[,] matrix, double scalar){
-        double[,] result = new double[,]{{1}};
+        double[,] result = new double[,]{{-1}};
         if ((matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2) || (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)){
             result = new double[matrix.GetLength(0), matrix.GetLength(0)];
             for (int i = 0; i < matrix.GetLength(0); i++){
