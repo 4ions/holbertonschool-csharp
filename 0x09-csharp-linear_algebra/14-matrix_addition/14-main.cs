fca584462b601c2c4b7db086a964a7de255ab6e6
4ions <leovalenciasantana@gmail.com>
2021-08-26 12:06:44 -0500

    Matrix addition

---
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-main.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program{
+    static void Main(string[] args)
+    {
+        double[,] matrix1 = {{14, -3, 0}, {-11, -5, 3}, {2, -9, 13}};
+        double[,] matrix2 = {{6, 16, 21}, {5, 2, 0}, {1, 3, 7}};
+
+        double[,] result = new double[3,3];
+        result = MatrixMath.Add(matrix1, matrix2);
+        for (int i = 0; i < result.GetLength(0); i++)
+        {
+            for (int j = 0; j < result.GetLength(0); j++){
+                System.Console.Write(result[i, j] + " ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
---
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+///<summary> Class MatrixMath</summary>
+class MatrixMath
+{
+    ///<summary> Method to add two matrixs</summary>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        double[,] result = new double[matrix1.GetLength(0),matrix1.GetLength(0)];
+        if (matrix1.GetLength(0) == matrix2.GetLength(0) && (matrix2.GetLength(0) == 3 || matrix1.GetLength(0) == 2)){
+            for (int i = 0; i < matrix1.GetLength(0); i++){
+                for (int j = 0; j < matrix1.GetLength(0); j++)
+                {
+                    result[i,j] = matrix1[i,j] + matrix2[i,j];
+                }
+            }
+        }
+        else {
+            double[,] error = new double[,] {{-1},{-1}};
+            return error;
+        }
+
+        return result;
+    }
+}
