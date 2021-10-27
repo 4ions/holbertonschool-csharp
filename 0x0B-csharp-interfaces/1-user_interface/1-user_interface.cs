86fff10f5bc38953724ed103e2887508f2c27a9b
4ions <leovalenciasantana@gmail.com>
2021-10-26 20:25:37 -0500

    Fix

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,35 +1,53 @@
 using System;
-
+// <summary>
+/// Ineterface Interactive
+/// </summary>
 interface IInteractive
 {
     void Interact();
 }
-
+/// <summary>
+/// Interface IBreakable
+/// </summary>
 interface IBreakable
 {
     int durability { get; set; }
     void Break();
 }
-
+/// <summary>
+/// Interface Collectable
+/// </summary>
 interface ICollectable
 {
     bool isCollected { get; set; }
     void Collect();
 }
-
+/// <summary>
+/// Test object class
+/// </summary>
 class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
     public int durability { get; set; }
     public bool isCollected { get; set; }
     public string name { get; set; }
+    /// <summary>
+    /// Interact inmplementation
+    /// </summary>
     public void Interact()
     {
 
     }
+    /// <summary>
+    /// Break implementation
+    /// </summary>
     public void Break()
     {
 
     }
+    // <summary>
+    /// isCollected
+    /// </summary>
+    /// <value>bool value</value>
     public void Collect()
     {
 
