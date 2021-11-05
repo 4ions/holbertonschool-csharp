4a5b284d6dd8f13900884048f747078731b9a4c0
4ions <leovalenciasantana@gmail.com>
2021-11-04 20:39:44 -0500

    tests

--- 0x0D-csharp-text_based_interface/InventoryManagement/BaseClass.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-class BaseClass
-{
-    public string id { get; set; }
-    public DateTime date_created { get; set; }
-    public DateTime date_updated { get; set; }
-
-}
--- 0x0D-csharp-text_based_interface/InventoryManagement/User.cs
+++
@@ -1,6 +0,0 @@
-using System;
-
-public class User: BaseClass
-{
-    public string name;
-}
