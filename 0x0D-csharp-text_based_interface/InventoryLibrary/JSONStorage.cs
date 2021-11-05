c831f965b20676d951be17c8c1c73dfe21ad4774
4ions <leovalenciasantana@gmail.com>
2021-11-04 20:36:32 -0500

    Json

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/JSONStorage.cs
@@ -0,0 +1,26 @@
+using System;
+
+/// <summary>Class where storage data</summary>
+class JSONStorage
+{
+    public Dictionary<ClassName> objects;
+    public Dictionary<id> objects;
+
+    public void All()
+    {
+        return (this.objects);
+    }
+
+    public void New(obj)
+    {
+    }
+
+    public void Save()
+    {
+    }
+
+    public void Load()
+    {
+    }
+}
+
